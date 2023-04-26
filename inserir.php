e33d728a461c0608815a0c04707ab86a764cd649
camila-reinert <camila_f_reinert@estudante.sesisenai.org.br>
2023-04-26 12:23:30 -0300

    first commit

---
+++ inserir.php
@@ -0,0 +1,13 @@
+<?php
+include("conecta.php");
+
+$matricula = $_POST["matricula"];
+$nome = $_POST["nome"];
+$idade = $_POST["idade"];
+
+$comando = $pdo->prepare("INSERT INTO alunos VALUES($matricula,'$nome',$idade)");
+
+$resultado = $comando->execute();
+
+header("Location: cadastro.html");
+?>
